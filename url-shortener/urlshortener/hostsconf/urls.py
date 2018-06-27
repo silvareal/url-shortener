9a2674e7c848309c7957f32a237a33f51377f9d7
silvareal <sylvernusakubo@yahoo.com>
2018-06-27 10:28:57 -0700

    duplicate removed

--- url-shortener/urlshortener/hostsconf/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from .views import wildcard_redirect
-
-urlpatterns = [
-    url(r'^(?P<path>.*)', wildcard_redirect ),
-]
--- url-shortener/urlshortener/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from shorten.views import ShortenCBV, KirrRedirectView
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', ShortenCBV.as_view()),
-    url(r'(?P<shortcode>[\w-]+)/$', KirrRedirectView.as_view(), name='shortcode')
-]
